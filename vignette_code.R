068dbd295537c2f95f85084b695024972c479a28
Graham Derryberry <g.derryberry@gmail.com>
2017-09-25 15:57:20 -0400

    Plot results from mod.val

--- vignette_code.R
+++ vignette_code.R
@@ -468,3 +468,58 @@ SongEvo2=SongEvo2_alt$res
 ts <- 36
 target.data <- subset(song.data, Population=="Schooner" & Year==2005)$Trill.FBW
 mod.val1 <- mod.val(summary.results=SongEvo2$summary.results, ts=ts, target.data=target.data)
+
+# Plot results from mod.val()
+# Similar to figure S8
+
+#Clear out plotting parameters from par.opt plots
+par(mfcol=c(1,1))
+par(mar=c(5, 4, 4, 2) + 0.1)
+par(cex=1)
+plot(SongEvo2$summary.results[1, , "trait.pop.mean"], xlab="Year", ylab="Bandwidth (Hz)", xaxt="n", type="n", xlim=c(-0.5, 36.5), ylim=c(min(SongEvo2$summary.results[, , "trait.pop.mean"], na.rm=TRUE), max(SongEvo2$summary.results[, , "trait.pop.mean"], na.rm=TRUE)))
+for(p in 1:iteration){
+  lines(SongEvo2$summary.results[p, , "trait.pop.mean"], col="light gray")
+}
+freq.mean <- apply(SongEvo2$summary.results[, , "trait.pop.mean"], 2, mean, na.rm=TRUE)
+lines(freq.mean, col="blue")
+axis(side=1, at=seq(0, 35, by=5), labels=seq(1970, 2005, by=5))#, tcl=-0.25, mgp=c(2,0.5,0))
+
+#Plot 95% quantiles 
+quant.means <- apply (SongEvo2$summary.results[, , "trait.pop.mean"], MARGIN=2, quantile, probs=c(0.95, 0.05), R=600, na.rm=TRUE)
+lines(quant.means[1,], col="blue", lty=2)
+lines(quant.means[2,], col="blue", lty=2)
+
+#plot mean and CI for historic songs.  
+#plot original song values
+library("boot")
+sample.mean <- function(d, x) {
+  mean(d[x])
+}
+boot_hist <- boot(starting.trait, statistic=sample.mean, R=100)
+ci.hist <- boot.ci(boot_hist, conf=0.95, type="basic")
+low <- ci.hist$basic[4]
+high <- ci.hist$basic[5]
+points(0, mean(starting.trait), pch=20, cex=0.6, col="black")
+library("Hmisc")
+errbar(x=0, y=mean(starting.trait), high, low, add=TRUE)
+
+#text and arrows
+text(x=5, y=2720, labels="Historical songs", pos=1)
+arrows(x0=5, y0=2750, x1=0.4, y1=mean(starting.trait), length=0.1)
+
+#plot current song values
+library("boot")
+sample.mean <- function(d, x) {
+  mean(d[x])
+}
+boot_curr <- boot(target.data, statistic=sample.mean, R=100)
+ci.curr <- boot.ci(boot_curr, conf=0.95, type="basic")
+low <- ci.curr$basic[4]
+high <- ci.curr$basic[5]
+points(years, mean(target.data), pch=20, cex=0.6, col="black")
+library("Hmisc")
+errbar(x=years, y=mean(target.data), high, low, add=TRUE)
+
+#text and arrows
+text(x=25, y=3100, labels="Current songs", pos=3)
+arrows(x0=25, y0=3300, x1=36, y1=mean(target.data), length=0.1)
