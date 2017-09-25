92e3315f8c52f6492972b11b80c3314dc22bdf70
Graham Derryberry <g.derryberry@gmail.com>
2017-09-25 11:09:02 -0400

    Translate README vignette into test code for debugging

---
+++ vignette_code.R
@@ -0,0 +1,10 @@
+##Vignette code from Readme
+
+#Load functions from files, keeping source references for future debugging
+
+SongEvo <- dget("SongEvo.R", TRUE)
+par.sens <- dget("par.sens.R", TRUE)
+par.opt <- dget("par.opt.R", TRUE)
+mod.val <- dget("mod.val.R", TRUE)
+h.test <- dget("h.test.R", TRUE)
+
